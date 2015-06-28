d4b245ac0c7b8ca607d41792b09248d6d5fe78c2
Khoa Le <khoaminhle5@gmail.com>
2015-06-28 15:26:13 -0500

    add image model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150628025629) do
+ActiveRecord::Schema.define(version: 20150628202451) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 20150628025629) do
     t.string   "level",       limit: 255
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string   "image_url",   limit: 255
+    t.integer  "exercise_id", limit: 4
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   create_table "posts", force: :cascade do |t|
