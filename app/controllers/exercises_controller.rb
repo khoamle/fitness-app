69b75807516112fdcc9cea9c1eb36284c411261a
Khoa Le <khoaminhle5@gmail.com>
2015-06-28 16:19:26 -0500

    add image text field on exercise form

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -10,12 +10,16 @@ class ExercisesController < ApplicationController
 
   def create
     @exercise = Exercise.create(id: params[:id], name: params[:name], instruction: params[:instruction], equipment: params[:equipment], muscle: params[:muscle], level: params[:level])
+    @image_1 = Image.create(exercise_id: @exercise.id, image_url: params[:image_1], exercise_id: @exercise.id)
+    @image_2 = Image.create(exercise_id: @exercise.id, image_url: params[:image_2], exercise_id: @exercise.id)
     @categorized_exercise = CategorizedExercise.create(category_id: params[:category][:category_id], exercise_id: @exercise.id)
+    @post = Post.create(exercise_id: @exercise.id, title: params[:title], body: params[:body])
     redirect_to "/exercises/#{@exercise.id}"
   end
 
   def show 
     @exercise = Exercise.find_by(id: params[:id])
+    p @exercise.images
   end
 
   def edit
@@ -25,6 +29,8 @@ class ExercisesController < ApplicationController
   def update
     @exercise = Exercise.find_by(id: params[:id])
     @exercise.update(id: params[:id], name: params[:name], instruction: params[:instruction], equipment: params[:equipment], muscle: params[:muscle], level: params[:level])
+    @image_1 = Image.create(exercise_id: @exercise.id, image_url: params[:image_1])
+    @image_2 = Image.create(exercise_id: @exercise.id, image_url: params[:image_2])
     redirect_to "/exercises/#{@exercise.id}"
   end
 
--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,5 +1,6 @@
 class Exercise < ActiveRecord::Base
   has_many :posts
+  has_many :images
   has_many :categorized_exercises
   has_many :categories, through: :categories_exercises
   belongs_to :user
